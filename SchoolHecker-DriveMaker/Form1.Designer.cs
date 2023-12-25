e10843f27c7821cfddc95a8894b6604982b78cb5
eetnaviation <117541874+eetnaviation@users.noreply.github.com>
2023-12-25 23:11:24 +0200

    Moveover from bitbucket

---
+++ SchoolHecker-DriveMaker/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace SchoolHecker_DriveMaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.drives = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.initBuild = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // drives
+            // 
+            this.drives.FormattingEnabled = true;
+            this.drives.Location = new System.Drawing.Point(10, 11);
+            this.drives.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.drives.Name = "drives";
+            this.drives.Size = new System.Drawing.Size(380, 21);
+            this.drives.TabIndex = 0;
+            this.drives.SelectedIndexChanged += new System.EventHandler(this.drives_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Standard",
+            "Pro",
+            "Ultimate"});
+            this.comboBox2.Location = new System.Drawing.Point(10, 56);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(380, 21);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 98);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(201, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(214, 98);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(176, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // initBuild
+            // 
+            this.initBuild.Location = new System.Drawing.Point(278, 151);
+            this.initBuild.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.initBuild.Name = "initBuild";
+            this.initBuild.Size = new System.Drawing.Size(110, 32);
+            this.initBuild.TabIndex = 5;
+            this.initBuild.Text = "BUILD";
+            this.initBuild.UseVisualStyleBackColor = true;
+            this.initBuild.Click += new System.EventHandler(this.initBuild_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 82);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(212, 82);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Product key";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 41);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Edition";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 197);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.initBuild);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.drives);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "SCHOOL-X DRIVE MAKER";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox drives;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button initBuild;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ SchoolHecker-DriveMaker/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace SchoolHecker_DriveMaker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitTimer();
+        }
+
+        private void drives_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fetchDrivesList()
+        {
+            var driveList = DriveInfo.GetDrives();
+            var currentDrives = new List<string>();
+
+            foreach (DriveInfo drive in driveList)
+            {
+                if (drive.DriveType == DriveType.Removable)
+                {
+                    currentDrives.Add(drive.Name);
+
+                    if (!drives.Items.Contains(drive.Name))
+                    {
+                        drives.Items.Add(drive.Name);
+                    }
+                }
+            }
+
+            for (int i = drives.Items.Count - 1; i >= 0; i--)
+            {
+                var driveName = (string)drives.Items[i];
+                if (!currentDrives.Contains(driveName))
+                {
+                    drives.Items.RemoveAt(i);
+                }
+            }
+
+            if (drives.Items.Count > 0)
+            {
+                drives.SelectedIndex = 0;
+            }
+            else
+            {
+                drives.SelectedIndex = -1;
+            }
+
+        }
+
+        public void InitTimer()
+        {
+            timer1 = new Timer();
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Interval = 2000; //milliseconds
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            fetchDrivesList();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void initBuild_Click(object sender, EventArgs e)
+        {
+            //Drive formatter
+            string chosenDrive = drives.SelectedItem.ToString();
+            if (MessageBox.Show("Are you sure you want to erase all data on chosen drive?", "Confirm!", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                
+            }
+            else
+            {
+                return;
+            }
+
+            // Key generation algorithm
+            string usernameNumeric = "";
+            try
+            {
+                string username = textBox1.Text;
+                usernameNumeric = getAlphabetNumberFromString(username);
+            }
+            catch (Exception error)
+            {
+                string finalError = error.ToString();
+                MessageBox.Show("Got error! Possible solution: Username textbox empty " + finalError);
+                return;
+            }
+            string allowedActions = "";
+            try
+            {
+                allowedActions = comboBox2.SelectedItem.ToString();
+                if (allowedActions == "Standard" || allowedActions == "Pro" || allowedActions == "Ultimate")
+                {
+
+                }
+                else
+                {
+                    MessageBox.Show("Invalid edition");
+                    return;
+                }
+            }
+            catch (Exception error)
+            {
+                string finalError = error.ToString();
+                MessageBox.Show("Got error! (Possible solution: No edition selected) " + finalError);
+                return;
+            }
+            var random = new Random();
+            string productKey = "";
+            if (allowedActions == "Standard")
+            {
+                productKey = random.Next(100, 201).ToString() + usernameNumeric;
+            }
+            else if (allowedActions == "Pro")
+            {
+                productKey = random.Next(300, 601).ToString() + usernameNumeric;
+            }
+            else if (allowedActions == "Ultimate")
+            {
+                productKey = random.Next(700, 901).ToString() + usernameNumeric;
+            }
+            string finalProductKey = dropCharacters(productKey);
+            textBox2.Text = finalProductKey;
+        }
+        
+        static string dropCharacters(string input)
+        {
+            //Drop all characters that are not numbers
+            string output = Regex.Replace(input, "[^.0-9]", "");
+            return output;
+        }
+
+        static int convertToNumbers(string input)
+        {
+            var ch = input[0];
+            char c = ch;
+            int index = char.ToUpper(c) - 64;
+            int finalIndex = index;
+            return finalIndex;
+        }
+
+        static string getAlphabetNumberFromString(string input)
+        {
+            string finalInput = getLetters(input);
+            int length = 5;
+            int trueLength = finalInput.Length;
+            if (trueLength < 5)
+            {
+                length = trueLength;
+            }
+            string finalSum = "";
+
+            for (int i = 0; i < length; i++)
+            {
+                int differentI = i - 1;
+                string differentIString = differentI.ToString();
+                string currentCharacter = finalInput.Substring(i, 1);
+
+                finalSum = finalSum + convertToNumbers(currentCharacter);
+            }
+            string convertedFinalSum = finalSum.ToString();
+            return convertedFinalSum;
+        }
+
+        static string getLetters(string input)
+        {
+            var myString = input;
+            var onlyLetters = new String(myString.Where(Char.IsLetter).ToArray());
+            return onlyLetters.ToString();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
